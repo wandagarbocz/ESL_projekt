f7ef2076e0e082143a9d907c6c0a582b49547386
wandagarbocz <59895661+wandagarbocz@users.noreply.github.com>
2021-12-16 22:07:50 +0100

    Add files via upload

--- sequencer2.py
+++ sequencer2.py
@@ -7,12 +7,12 @@ def sequencer(gain, shutdown, reset, clk, ceo, start):
     t_state = enum('A_5', 'Fis_5', 'G_5', 'A_4', 'B_4', 'Cis_5', 'D_5', 'E_5')
     state = Signal(t_state.A_5)
     sounds = [33, 43, 38, 64, 60, 57, 53, 48]#[11, 13, 12, 22, 19, 18, 17, 15]
-    sounds_divider = [Signal(modbv(1, 0, sounds[i])[7:]) for i in range(len(sounds))]
+    sounds_divider = [Signal(modbv(1, min=0, max=sounds[i])[7:]) for i in range(len(sounds))]
     length = [31, 17, 17, 17, 17, 17, 17, 17, 17]#[46, 19, 21, 11, 13, 14, 15, 17, 23]
-    length_divider = [Signal(modbv(1, 0, length[i])[5:]) for i in range(len(length))]
+    length_divider = [Signal(modbv(1, min=0, max=length[i])[5:]) for i in range(len(length))]
     ceo_sig = Signal(bool(0))
     #iteration = Signal(modbv(13, min=13, max=16))
-    iteration = Signal(intbv(0, 0, 2)[2:])
+    iteration = Signal(modbv(0, min=0, max=2)[2:])
 
 
     @always_seq(clk.posedge, reset=reset)
